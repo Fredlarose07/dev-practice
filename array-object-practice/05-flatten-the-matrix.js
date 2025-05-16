8642b241bc5cd10ae98726eedc3b142f04191f84
Yann <yann.duchateau97@gmail.com>
2025-05-16 14:34:24 +0200

    fix: flattenTheMatrix

--- array-object-practice/05-flatten-the-matrix.js
+++ array-object-practice/05-flatten-the-matrix.js
@@ -1,23 +1,22 @@
 const nestedArray = [1, [2, [3, [4, 5]]]];
 
-function flattenTheMatrix(){
-
-    let array = []
     
 
-    for(i = 0; i < nestedArray.length; i++){
-
-        if(typeof nestedArray[i] === "number"){
-            array.push(nestedArray[i])
-        } else{
-            nestedArray[i].forEach(element => {
-                array.push(element)
-            });
+function flattenTheMatrix(arr, array = []){
+    
+    
+    arr.forEach(element => {
+        if(Array.isArray(element)){
+            flattenTheMatrix(element, array)
+        } else {
+            array.push(element)
         }
-    } 
-    
-    console.log(nestedArray[0], nestedArray[1], nestedArray[1][1])
+    });
 
+    
+    return array
 }
 
-flattenTheMatrix()
+
+console.log(flattenTheMatrix(nestedArray))
+
