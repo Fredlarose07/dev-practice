a014e6975f70e2ab7a4ecd97076358ac30220485
Yann <yann.duchateau97@gmail.com>
2025-05-22 21:50:09 +0200

    todolist

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -26,3 +26,49 @@ function counter(){
 }
 
 counter()
+
+// ========== TodoList ==========
+
+function TodoList(){
+
+const input = document.getElementById("todo-input");
+const addBtn = document.getElementById("add-btn");
+const todoList = document.getElementById("todo-list");
+const achieveTodo = document.getElementById("achieveTodo");
+
+
+addBtn.addEventListener("click", () => {
+  const taskText = input.value.trim();
+
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  // ✅ Clic sur l'élément => barré / décoché
+  li.addEventListener("click", () => {
+
+    li.classList.toggle("done");
+    
+       if(li.classList == "done"){
+        achieveTodo.appendChild(li);
+        console.log("done")
+    } else {
+        todoList.appendChild(li);
+        console.log("pas done")
+    }
+    
+
+    
+});
+
+
+  todoList.appendChild(li);
+  input.value = "";
+});
+
+
+}
+
+
+TodoList()
