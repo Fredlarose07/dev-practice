9352176f05b1d1f729f77cfdb7979ac45b832e61
Yann <yann.duchateau97@gmail.com>
2025-05-23 23:24:25 +0200

    feat: todo list

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -42,8 +42,25 @@ addBtn.addEventListener("click", () => {
 
   if (taskText === "") return;
 
-  const li = document.createElement("li");
-  li.textContent = taskText;
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    const suppBtn = document.createElement("span");
+    suppBtn.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" 
+            width="16" height="16" viewBox="0 0 24 24" 
+            fill="none" stroke="currentColor" stroke-width="2" 
+            stroke-linecap="round" stroke-linejoin="round" 
+            class="feather feather-trash">
+            <polyline points="3 6 5 6 21 6"></polyline>
+            <path d="M19 6l-2 14H7L5 6"></path>
+            <path d="M10 11v6"></path>
+            <path d="M14 11v6"></path>
+        </svg>
+        `;
+    suppBtn.classList.add("iconBtn")
+    
+    
 
   // ✅ Clic sur l'élément => barré / décoché
   li.addEventListener("click", () => {
@@ -57,13 +74,11 @@ addBtn.addEventListener("click", () => {
         todoList.appendChild(li);
         console.log("pas done")
     }
-    
-
-    
 });
 
-
+  
   todoList.appendChild(li);
+  li.appendChild(suppBtn)
   input.value = "";
 });
 
