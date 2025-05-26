b891d1e1809aab5e58e8890baa6f7b0f7ee4029f
Yann <yann.duchateau97@gmail.com>
2025-05-26 15:54:54 +0200

    add style on TodoList

--- dom-manipulation/todo-list/script.js
+++ dom-manipulation/todo-list/script.js
@@ -70,28 +70,30 @@ function createTask(){
     // Je récupére le input, la liste de tache, et le bouton pour envoyer la tache. 
     const taskList = document.getElementById('taskList')
     const taskInput = document.getElementById('taskInput')
-    const addTaskBtn = document.getElementById('addTaskBtn')
 
     
     // quand je clique sur le btn alors je crée une tache et je lui ajoute en contenu la value de mon input
     // je crée également un btn pour supprimer la tache sur chaque task, via innerHTML j'ajoute le svg et j'ajoute aussi une classe de style
     // La liste (taskList) a un enfant li (task) et (task) a un enfant (removeBtn)
-    addTaskBtn.addEventListener("click", () => {
+    taskInput.addEventListener("keydown", (event) => {
 
-        const task = document.createElement('li')
-        task.textContent = taskInput.value.trim()
-        const removeBtn = document.createElement('span')
-        removeBtn.innerHTML = `<svg width="16px" height="16px" viewBox="0 0 24.00 24.00" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(0)matrix(1, 0, 0, 1, 0, 0)" stroke="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.384"></g><g id="SVGRepo_iconCarrier"> <path d="M17 12C17 11.4477 16.5523 11 16 11H8C7.44772 11 7 11.4477 7 12C7 12.5523 7.44771 13 8 13H16C16.5523 13 17 12.5523 17 12Z" fill="#0F0F0F"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 23C18.0751 23 23 18.0751 23 12C23 5.92487 18.0751 1 12 1C5.92487 1 1 5.92487 1 12C1 18.0751 5.92487 23 12 23ZM12 20.9932C7.03321 20.9932 3.00683 16.9668 3.00683 12C3.00683 7.03321 7.03321 3.00683 12 3.00683C16.9668 3.00683 20.9932 7.03321 20.9932 12C20.9932 16.9668 16.9668 20.9932 12 20.9932Z" fill="#363636"></path> </g></svg>`
-        removeBtn.classList.add("iconBtn")
-        taskList.appendChild(task)   
-        task.appendChild(removeBtn)
+        if(event.key === "Enter"){
 
-        removeTask(removeBtn, task)
-    })
+            if (taskInput.value.trim() === "") return;
 
-
-    
+                const task = document.createElement('li')
+                task.textContent = taskInput.value.trim()
+                const removeBtn = document.createElement('span')
+                removeBtn.innerHTML = `<svg width="16px" height="16px" viewBox="0 0 24.00 24.00" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(0)matrix(1, 0, 0, 1, 0, 0)" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.384"></g><g id="SVGRepo_iconCarrier"> <path d="M17 12C17 11.4477 16.5523 11 16 11H8C7.44772 11 7 11.4477 7 12C7 12.5523 7.44771 13 8 13H16C16.5523 13 17 12.5523 17 12Z" fill="#FFFFFF"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 23C18.0751 23 23 18.0751 23 12C23 5.92487 18.0751 1 12 1C5.92487 1 1 5.92487 1 12C1 18.0751 5.92487 23 12 23ZM12 20.9932C7.03321 20.9932 3.00683 16.9668 3.00683 12C3.00683 7.03321 7.03321 3.00683 12 3.00683C16.9668 3.00683 20.9932 7.03321 20.9932 12C20.9932 16.9668 16.9668 20.9932 12 20.9932Z" fill="#FFFFFF"></path> </g></svg>`
+                removeBtn.classList.add("iconBtn")
+                taskList.appendChild(task)   
+                task.appendChild(removeBtn)
+                
+                removeTask(removeBtn, task)
+        }
+    }) 
 }
+
 
 createTask()
 
